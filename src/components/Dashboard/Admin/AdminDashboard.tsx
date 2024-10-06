316c659ba0da1b8998e1026cc1056e6c92891cda
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2024-10-06 23:57:32 +0600

    add product

--- src/components/Dashboard/Admin/AdminDashboard.tsx
+++ src/components/Dashboard/Admin/AdminDashboard.tsx
@@ -1,9 +1,11 @@
 const AdminDashboard = () => {
-	return (
-		<div>
-			<p>AdminDashboard</p>
-		</div>
-	);
+  return (
+    <div>
+      <p className="text-2xl font-bold text-center mt-10">
+        Welcome to Admin Dashboard
+      </p>
+    </div>
+  );
 };
 
 export default AdminDashboard;
