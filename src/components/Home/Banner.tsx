c9bd1ebb282fb5c3d794850172ae1e7d1feb99ff
Chowdhury Tafsir Ahmed Siddiki <ctafsiras@gmail.com>
2024-10-07 14:27:47 +0600

    banner responsive done

--- src/components/Home/Banner.tsx
+++ src/components/Home/Banner.tsx
@@ -4,22 +4,24 @@ import { FaPhone } from "react-icons/fa";
 const Banner = () => {
   return (
     <section className="space-y-6">
-      <div className="flex justify-between items-center mt-4">
-        <div className="sm:w-1/2 px-8 space-y-5">
-          <p className="text-[#FF6F00] text-xl capitalize tracking-[.4em]">
+      <div className="flex flex-col md:flex-row justify-between items-center mt-4">
+        <div className="w-full md:w-1/2 px-4 md:px-8 space-y-4 md:space-y-5 text-center md:text-left">
+          <p className="text-[#FF6F00] text-lg md:text-xl capitalize tracking-[.3em] md:tracking-[.4em]">
             BAKERY & PASTRY
           </p>
-          <h1 className="text-5xl">Freshly Baked All Day and Every Day</h1>
-          <p className="text-gray-500 text-lg">
+          <h1 className="text-3xl md:text-5xl">
+            Freshly Baked All Day and Every Day
+          </h1>
+          <p className="text-gray-500 text-base md:text-lg">
             Indulge in our artisanal breads and pastries, crafted with passion
             and the finest ingredients. Experience the warmth and aroma of
             freshly baked goods that will delight your senses.
           </p>
-          <div className="flex gap-x-3 mb-5">
-            <button className="bg-[#FF6F00] text-white px-5 py-2 hover:bg-black transition duration-300">
+          <div className="flex flex-col sm:flex-row gap-y-3 sm:gap-x-3 mb-5">
+            <button className="bg-[#FF6F00] text-white px-4 py-2 hover:bg-black transition duration-300 w-full sm:w-auto">
               DISCOVER MORE
             </button>
-            <button className="text-[#FF6F00] px-3 py-2 hover:text-black transition duration-300 flex items-center gap-x-2">
+            <button className="text-[#FF6F00] px-3 py-2 hover:text-black transition duration-300 flex items-center justify-center sm:justify-start gap-x-2 w-full sm:w-auto">
               CONTACT US <FaPhone />
             </button>
           </div>
